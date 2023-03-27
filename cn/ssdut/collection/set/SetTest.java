ceab0f57239e74a6387e935412ed7a0baea0fb6e
WangShiYao666 <1170309039@qq.com>
2023-03-27 16:56:14 +0800

    测试E610

--- cn/ssdut/collection/set/SetTest.java
+++ cn/ssdut/collection/set/SetTest.java
@@ -1,7 +1,10 @@
 package cn.ssdut.collection.set;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SetTest {
     public static void main(String[] args) {
-
+        Map map = new HashMap();
     }
 }
