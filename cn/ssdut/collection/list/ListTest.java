94b086ce459ee913b1cb8d3997b5ba17d685358d
WangShiYao666 <1170309039@qq.com>
2023-03-27 14:33:45 +0800

    第九章——列表

---
+++ cn/ssdut/collection/list/ListTest.java
@@ -0,0 +1,10 @@
+package cn.ssdut.collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+    }
+}
